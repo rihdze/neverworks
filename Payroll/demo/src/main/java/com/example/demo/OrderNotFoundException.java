2d2199ee0a6d65bc120332b66d349fe25def9c7c
Rihards Dzerkals <rih.dze@gmail.com>
2021-11-06 22:28:22 +0200

    lol

---
+++ Payroll/demo/src/main/java/com/example/demo/OrderNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class OrderNotFoundException extends RuntimeException{
+
+    OrderNotFoundException(Long id){
+        super("Could not find order with id :" + id);
+    }
+
+
+
+}
